b364d32e614fd3f660c17c0dbb1c9e662ebee1b0
Enes Doğanoğlu <117980244+enesdoganoglu@users.noreply.github.com>
2023-11-19 23:43:05 +0300

    Update 301-jibri.sh

--- installer-sub-scripts/eb-jibri/301-jibri.sh
+++ installer-sub-scripts/eb-jibri/301-jibri.sh
@@ -110,6 +110,7 @@ EOS
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 export DEBIAN_FRONTEND=noninteractive
+apt-get $APT_PROXY -y install jq
 apt-get $APT_PROXY -y install gnupg unzip unclutter
 apt-get $APT_PROXY -y install libnss3-tools
 apt-get $APT_PROXY -y install va-driver-all vdpau-driver-all
@@ -150,7 +151,15 @@ EOS
 lxc-attach -n $MACH -- zsh <<EOS
 set -e
 CHROME_VER=\$(dpkg -s google-chrome-stable | egrep "^Version" | \
-    cut -d " " -f2 | cut -d. -f1)
+    cut -d " " -f2 | cut -d. -f1-3)
+CHROMELAB_LINK="https://googlechromelabs.github.io/chrome-for-testing"
+CHROMEDRIVER_LINK=\$(curl -s \
+    \$CHROMELAB_LINK/known-good-versions-with-downloads.json | \
+    jq -r ".versions[].downloads.chromedriver | select(. != null) | .[].url" | \
+    grep linux64 | grep "\$CHROME_VER" | tail -1)
+wget -T 30 -qO /tmp/chromedriver_linux64.zip \$CHROMEDRIVER_LINK
+unzip -o /tmp/chromedriver-linux64.zip -d /tmp
+mv /tmp/chromedriver-linux64/chromedriver /usr/local/bin/
     
 CHROME_VER=\$(dpkg -s google-chrome-stable | egrep "^Version" | \
     cut -d " " -f2 | cut -d. -f1-3)
